336f63311cb538b97208ec2231b5467a0b552ce0
Tobias Heine <github@c5y.de>
2026-08-12 22:28:54 +0200

    Add files via upload

---
+++ Zip-Dependency/Zip/minizip/include/Minizip.h
@@ -0,0 +1,15 @@
+//
+//  Minizip.h
+//  Zip
+//
+//  Created by Florian Friedrich on 3/27/19.
+//  Copyright © 2019 Roy Marmelstein. All rights reserved.
+//
+
+#ifndef Minizip_h
+#define Minizip_h
+
+#import "unzip.h"
+#import "zip.h"
+
+#endif /* Minizip_h */
